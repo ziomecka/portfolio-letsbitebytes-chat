351e2377d10d344c72ae705d618fbe0e945c44f5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-08-30 10:14:26 +0200

    Adds components exports

---
+++ src/front/components/index.ts
@@ -0,0 +1,5 @@
+export { Conversation } from './Conversation/';
+export { Login } from './Login';
+export { ProtectedRoute } from './ProtectedRoute/';
+export { Trainee } from './Trainee';
+export { Trainer } from './Trainer';
