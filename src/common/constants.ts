b1a3f80069c8f37f522feaeeecf6b3119433fe3f
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-08-30 10:02:13 +0200

    Common | Adds common constants

---
+++ src/common/constants.ts
@@ -0,0 +1,8 @@
+export const HTML_ROOT_ID = 'root'
+export const WINDOW_INITIAL_STATE = '__INITIAL_STATE__';
+
+export const AppRoutes = {
+  loginRoute: ServerRoutes.loginRoute,
+  publicRoute: ServerRoutes.publicRoute,
+  protectedRoute: '/protected',
+};
