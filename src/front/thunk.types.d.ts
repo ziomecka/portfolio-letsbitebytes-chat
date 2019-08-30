3d65aac3151a972ac1f108127d4cbe066bd241d9
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-08-30 10:45:27 +0200

    Front | Adds thunk types

---
+++ src/front/thunk.types.d.ts
@@ -0,0 +1,3 @@
+declare type AppThunkAction<A, R = A > = import('redux-thunk').ThunkAction<Promise<R>, AppState, {api: Api}, A>;
+
+declare type AppThunkDispatch<A> = import('redux-thunk').ThunkDispatch<AppState, {api: Api}, A>;
