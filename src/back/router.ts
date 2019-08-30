df0c5d5776cc6057498ec283d7452129ed97a721
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-08-30 10:26:58 +0200

    Baack | Adds router

---
+++ src/back/router.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import * as path from 'path';
+import { login } from './login/';
+import { serveHtml } from './ssr/';
+
+require('dotenv').config();
+
+const { env: { NODE_ENV } } = process;
+
+const FRONT_URL = NODE_ENV === 'production'
+  ? ''
+  : path.resolve(__dirname, '../../dist/front');
+
+export const router = (app: import('express').Application): void => {
+  app.use(express.static(FRONT_URL));
+  app.use(ServerRoutes.loginRoute, login);
+  app.use(ServerRoutes.publicRoute, serveHtml);
+};
