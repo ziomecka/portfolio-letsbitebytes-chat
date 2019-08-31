902a2bb59ff3e8e825bd7d9045ffe3e7a5c6a1c4
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-08-31 12:40:09 +0200

    Front | uses socketReducer in appReducer

--- src/front/reducer.ts
+++ src/front/reducer.ts
@@ -1,13 +1,13 @@
 import { INITIAL_STATE } from './initial-state';
 import { Reducer } from 'redux';
 import { commonStateReducer } from './duck/';
-import { conversationsReducer } from './components/Conversations/';
 import { loginReducer } from './components/Login/'
+import { socketReducer } from './components/Socket/';
 
 const appReducer: Reducer<AppState, AppAction> = (state = INITIAL_STATE, action) => {
   return {
     user: loginReducer(state.user, action),
-    conversations: conversationsReducer(state.conversations, action),
+    conversations: socketReducer(state.conversations, action),
     ...commonStateReducer({ activeConversation: state.activeConversation }, action),
   };
 };
