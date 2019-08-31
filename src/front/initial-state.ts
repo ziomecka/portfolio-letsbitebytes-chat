9f3877f5b2ea5b653f8531d9a57241938e55e2a7
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-08-31 12:35:56 +0200

    Front | adds default initial state on apps level. Removes initial state from Login component

--- src/front/initial-state.ts
+++ src/front/initial-state.ts
@@ -1,5 +1,29 @@
 import { WINDOW_INITIAL_STATE } from '../common/';
 
+const email = 'initialEmail';
+const isAuthenticated = false;
+const login = 'initialLogin';
+const password = 'initialPassword';
+const role = 'initialRole';
+
+const conversations: Statement[] = [];
+const activeConversation = '';
+
+const DEFAULT_INITIAL_STATE = {
+  user: {
+    email,
+    isAuthenticated,
+    login,
+    password,
+    role,
+  },
+  conversations,
+  activeConversation
+};
+
 export const INITIAL_STATE = (
-  (window as AppWindow)[ WINDOW_INITIAL_STATE ] as unknown as AppState
+  {
+    ...DEFAULT_INITIAL_STATE,
+    ...(window as AppWindow)[ WINDOW_INITIAL_STATE ] as unknown as AppState
+  }
 );
