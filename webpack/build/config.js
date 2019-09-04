949b6127072c014e071a2178b4269622cca06ca2
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-04 10:03:22 +0200

    Config | adds sass loader to webpack build config

--- webpack/build/config.js
+++ webpack/build/config.js
@@ -1,3 +1,12 @@
+const postcssFlexbugs = require('postcss-flexbugs-fixes');
+const postcssLost = require('lost');
+const postcssImport = require('postcss-import');
+const postcssNext = require('postcss-cssnext');
+const cssDeclarationSorter = require('css-declaration-sorter');
+const cssMqpacker = require('css-mqpacker');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: 'development',
   resolve: {
@@ -15,6 +24,49 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.sass$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                postcssFlexbugs(),
+                postcssLost(),
+                postcssImport(),
+                postcssNext({
+                  browsers: ['last 2 version', 'Safari 7', 'ie 10'],
+                }),
+                cssDeclarationSorter({
+                  order: 'concentric-css',
+                }),
+                cssMqpacker(),
+              ],
+              sourceMap: false,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: false
+            }
+          },
+        ],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
