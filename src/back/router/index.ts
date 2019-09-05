aeb5a999bc127672d5b58741670a34f86e75da8a
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-05 11:01:16 +0200

    Back | uses middleware only if query

--- src/back/router/index.ts
+++ src/back/router/index.ts
@@ -2,13 +2,14 @@ import * as express from 'express';
 import { BUNDLE_PATH } from '../constants';
 import { login } from './login';
 import { logout } from './logout';
+import { onlyIfQuery } from './only-if-query';
 import { serveHtml } from './ssr/';
 import { serveJSFiles } from './js-files';
 
 export const router = (app: ExpressApplication): void => {
   app.get('*.js', serveJSFiles);
   app.use(express.static(BUNDLE_PATH));
-  app.get(ServerRoutes.loginRoute, login);
-  app.get(ServerRoutes.logoutRoute, logout);
+  app.get(ServerRoutes.loginRoute, onlyIfQuery(login));
+  app.get(ServerRoutes.logoutRoute, onlyIfQuery(logout));
   app.get(ServerRoutes.publicRoute, serveHtml);
 };
