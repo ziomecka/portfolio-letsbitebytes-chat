cc82a373b9c7178fc0ffdc49d74f35e863d02db1
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-05 08:41:47 +0200

    Back | adds logger to socket connection handler

--- src/back/socket/handlers/connection-handler.ts
+++ src/back/socket/handlers/connection-handler.ts
@@ -1,16 +1,29 @@
 import { User } from '../../user/';
 import { disconnectHandler } from './disconnect-handler';
+import { logger } from '../../logger/';
+
+const log = logger('socket');
 
 export const connectionHandler = async (socket: Socket): Promise<void> => {
   const { handshake: { query: { login = '' } = { } }, id } = socket;
 
+  log.info('User connected: ', login, ', ', id);
+
   try {
     if (login) {
       await User.storeUser(login, id);
+      log.info('User stored: ', login, ', ', id);
     }
+  } catch {
+    log.warn('User not stored: ', id);
+    // TODO
   } finally {
     socket.emit(SocketMessages.userConnected, { login }); // TODO
 
+    log.info('User informed about connection: ', login, ', ', id);
+
     socket.on(SocketMessages.disconnect, () => disconnectHandler(socket));
+
+    log.info('Disconnet listener registered for: ', socket.id, login);
   }
 };
