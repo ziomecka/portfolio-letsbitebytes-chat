b2e25693a969d92141fbbca08ec76a5d4396627b
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-05 12:50:03 +0200

    Common | overrides margin and padding in theme

--- src/common/theme/other.ts
+++ src/common/theme/other.ts
@@ -1,9 +1,22 @@
 import {
   BUTTON_WIDTH,
+  MARGIN,
+  MARGIN_XL,
+  PADDING,
+  PADDING_XL,
 } from './other-constants';
 import {
   flexColumnJustifyFlexStartAlignLeft,
 } from './flexes';
+
+const buildMarginPadding = (margin = MARGIN, padding = PADDING): unknown => ({
+  margin,
+  marginLeft: 0,
+  padding,
+  paddingLeft: 0,
+});
+
+const marginPadding = buildMarginPadding();
 
 const other = {
   props: {
@@ -26,18 +39,28 @@ const other = {
       root: {
         justifyContent: 'flex-start',
         maxWidth: BUTTON_WIDTH,
+        ...marginPadding,
+      },
+    },
+    MuiInputBase: {
+      root: {
+        ...marginPadding,
       },
     },
     MuiPaper: {
+      // @ts-ignore
       root: {
         ...flexColumnJustifyFlexStartAlignLeft,
-        boxSizing: 'border-box'
+        boxSizing: 'border-box',
+        margin: MARGIN_XL,
+        padding: PADDING_XL,
       }
     },
     MuiTypography: {
       root: {
         textAlign: 'left',
-        width: '100%'
+        width: '100%',
+        ...buildMarginPadding(MARGIN_XL, PADDING_XL),
       },
     },
   }
