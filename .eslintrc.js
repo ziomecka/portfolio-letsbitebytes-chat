abea30b41654a07bb2df110d2d4928b4dc2107e6
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 19:05:25 +0200

    Config | uses single quotes in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,57 +2,57 @@ module.exports = {
   env: {
       es6: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
       ecmaVersion: 6,
-      sourceType: "module",
+      sourceType: 'module',
       ecmaFeatures: {
           jsx: true
       },
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
   ],
   plugins: [
-    "@typescript-eslint",
-    "react"
+    '@typescript-eslint',
+    'react'
   ],
   rules: {
-      "@typescript-eslint/ban-ts-ignore": "off",
-      "@typescript-eslint/no-explicit-any": "error",
-      "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/explicit-member-accessibility": ["error", {
-        accessibility: "explicit",
+      '@typescript-eslint/ban-ts-ignore': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-member-accessibility': ['error', {
+        accessibility: 'explicit',
         overrides: {
-          constructors: "no-public"
+          constructors: 'no-public'
         }
       }],
-      "array-bracket-spacing": ["error", "always", { "singleValue": false }],
-      "arrow-spacing": "error",
-      "comma-dangle": ["error", "always-multiline"],
-      "comma-spacing": "error",
-      "computed-property-spacing": ["error", "always"],
-      "eol-last": ["error", "always"],
-      "indent": ["error", 2, { SwitchCase: 1 }],
-      "jsx-quotes": ["error", "prefer-double"],
-      "key-spacing": "error",
-      "max-len": ["error", { "code": 100 }],
-      "no-console": "error",
-      "no-duplicate-imports": "error",
-      "no-multi-spaces": "error",
-      "no-undef": "off",
-      "object-curly-spacing": ["error", "always"],
-      "quotes": ["error", "single"],
-      "react/jsx-uses-react": 1,
-      "semi": "error",
-      "sort-imports": "error",
-      "space-before-function-paren": "error",
-      "space-in-parens": ["error", "always"],
-      "template-curly-spacing": ["error", "always"],
-      "@typescript-eslint/no-unused-vars": "off",
+      'array-bracket-spacing': ['error', 'always', { 'singleValue': false }],
+      'arrow-spacing': 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      'comma-spacing': 'error',
+      'computed-property-spacing': ['error', 'always'],
+      'eol-last': ['error', 'always'],
+      'indent': ['error', 2, { SwitchCase: 1 }],
+      'jsx-quotes': ['error', 'prefer-double'],
+      'key-spacing': 'error',
+      'max-len': ['error', { 'code': 100 }],
+      'no-console': 'error',
+      'no-duplicate-imports': 'error',
+      'no-multi-spaces': 'error',
+      'no-undef': 'off',
+      'object-curly-spacing': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'react/jsx-uses-react': 1,
+      'semi': 'error',
+      'sort-imports': 'error',
+      'space-before-function-paren': 'error',
+      'space-in-parens': ['error', 'always'],
+      'template-curly-spacing': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': 'off',
   }
 }
