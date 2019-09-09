63e14113e2deca8c4308cd703dc942a631504807
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 17:26:23 +0200

    Back | declares ExpressNext type

--- src/back/router/_types.d.ts
+++ src/back/router/_types.d.ts
@@ -4,4 +4,6 @@ declare type ExpressRequest = import('express').Request;
 
 declare type ExpressResponse = import('express').Response;
 
+declare type ExpressNext = import('express').NextFunction;
+
 declare type Middleware = (req: ExpressRequest, res: ExpressResponse, next: NextFunction) => void;
