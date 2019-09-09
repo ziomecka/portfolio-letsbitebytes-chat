de1774822439e9794ea770fe53f8899e827ba07e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 14:08:57 +0200

    Common | adds shape with borderRadius to theme

--- src/common/theme/index.ts
+++ src/common/theme/index.ts
@@ -2,11 +2,13 @@ import createMuiTheme, { ThemeOptions } from '@material-ui/core/styles/createMui
 import { Theme } from '@material-ui/core';
 import { other } from './other';
 import { palette } from './palette';
+import { shape } from './shape';
 import { typography } from './typography';
 
 const getTheme = (): Theme => {
   return createMuiTheme({
     palette,
+    shape,
     typography,
     ...other,
   } as ThemeOptions);
