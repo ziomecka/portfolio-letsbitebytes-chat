10ddc7f3300aa53209a44ac65b32fa97ce98f577
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 20:31:37 +0200

    Revert "TO BE REVERTED | adds console.log"
    This reverts commit 7041825ee07fe50133ff150d52025876c0be28a7.

--- src/back/router/middlewares/constants.ts
+++ src/back/router/middlewares/constants.ts
@@ -1,14 +1,10 @@
 import * as path from 'path';
 import { NODE_ENV } from '../../constants';
-import { logger } from '../../logger/'
-export { NODE_ENV };
 
-const log = logger();
+export { NODE_ENV };
 
 const root = '../../../../';
 
 export const BUNDLE_PATH = NODE_ENV === 'production'
   ? path.resolve(__dirname, root, '../deploy/')
   : path.resolve(__dirname, root, 'build/');
-
-log.info('BUNDLE_PATH', BUNDLE_PATH);
