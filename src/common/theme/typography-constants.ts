94554e77c4bd6ae9c8815b6da8dcef10032d3f83
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 14:08:14 +0200

    Common | uses createTypography function for theme. Deletes unused typography levels, adds general font styling and button styling

--- src/common/theme/typography-constants.ts
+++ src/common/theme/typography-constants.ts
@@ -1,20 +1,26 @@
 export const fontSizes = {
-  h1: {
-    fontSize: "3rem",
-  },
-  h2: {
-    fontSize: "2rem",
-  },
-  h3: {
-    fontSize: "1.8rem",
-  },
-  h4: {
-    fontSize: "1.4rem",
-  },
-  h5: {
-    fontSize: "1.2rem",
-  },
-  h6: {
-    fontSize: "1rem",
-  },
+  h1: '1.5rem',
+  h2: '1.3rem',
+  h3: '1.2rem',
+  button: '1.1em',
 };
+
+export const fontFamily = '"Roboto", "Helvetica", "Arial", sans-serif';
+
+export const fontSize = 16;
+
+export const fontWeightLight = 400;
+export const fontWeightMedium = 500;
+export const fontWeightRegular = 500;
+export const fontWeightBold = 600;
+
+export const lineHeight = '1.5em';
+
+export const fontWeightHeading = 400;
+export const letterSpacingHeading = '-.02em';
+export const lineHeightHeading = '1.3em';
+
+export const fontWeightButton = 400;
+export const letterSpacingButton = '-.01em';
+export const lineHeightButton = '1.5em';
+export const textTransformButton = 'none';
--- src/common/theme/typography.ts
+++ src/common/theme/typography.ts
@@ -1,7 +1,57 @@
-import { fontSizes } from './typography-constants';
+import {
+  fontFamily,
+  fontSize,
+  fontSizes,
+  fontWeightBold,
+  fontWeightButton,
+  fontWeightHeading,
+  fontWeightLight,
+  fontWeightMedium,
+  fontWeightRegular,
+  letterSpacingButton,
+  letterSpacingHeading,
+  lineHeightButton,
+  lineHeightHeading,
+  textTransformButton,
+} from './typography-constants';
+import createTypography from '@material-ui/core/styles/createTypography';
+import { palette } from './palette';
 
-const typography = {
-  ...fontSizes,
-};
+const { h1, h2, h3, button } = fontSizes;
+
+const typography = createTypography(palette, {
+  fontFamily,
+  fontSize,
+  fontWeightMedium,
+  fontWeightLight,
+  fontWeightRegular,
+  fontWeightBold,
+  h1: {
+    fontSize: h1,
+    fontWeight: fontWeightHeading,
+    lineHeight: lineHeightHeading,
+    letterSpacing: letterSpacingHeading,
+  },
+  h2: {
+    fontSize: h2,
+    fontWeight: fontWeightHeading,
+    lineHeight: lineHeightHeading,
+    letterSpacing: letterSpacingHeading,
+  },
+  h3: {
+    fontSize: h3,
+    fontWeight: fontWeightHeading,
+    lineHeight: lineHeightHeading,
+    letterSpacing: letterSpacingHeading,
+  },
+  button: {
+    fontSize: button,
+    textTransform: textTransformButton,
+    fontFamily,
+    lineHeight: lineHeightButton,
+    letterSpacing: letterSpacingButton,
+    fontWeight: fontWeightButton,
+  },
+});
 
 export { typography };
