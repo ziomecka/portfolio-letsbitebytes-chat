857f2607bbbacc92713f4f4d0e2f7922b152495e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 17:17:19 +0200

    Back | sends development js-files from the middleware, without express.static

--- src/back/router/middlewares/js-files.ts
+++ src/back/router/middlewares/js-files.ts
@@ -1,15 +1,16 @@
 import * as path from 'path';
 import {
   BUNDLE_PATH,
-  NODE_ENV
+  NODE_ENV,
 } from '../../constants';
 
-export const serveJSFiles = ( req: ExpressRequest, res: ExpressResponse, next: NextFunction): void => {
+export const serveJSFiles = ( req: ExpressRequest, res: ExpressResponse): void => {
+  res.set('Content-Type', 'text/javascript');
+
   if ( NODE_ENV === 'production' ) {
     res.set('Content-Encoding', 'gzip');
-    res.set('Content-Type', 'text/javascript');
-
-    req.url = path.resolve(BUNDLE_PATH, `${ req.url }.gz`);
+    res.sendFile(path.resolve(BUNDLE_PATH, `${ req.url }.gz`));
+  } else {
+    res.sendFile(path.join(BUNDLE_PATH, req.url));
   }
-  next();
 };
