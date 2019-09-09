7041825ee07fe50133ff150d52025876c0be28a7
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 19:57:08 +0200

    TO BE REVERTED | adds console.log

--- src/back/router/middlewares/constants.ts
+++ src/back/router/middlewares/constants.ts
@@ -1,10 +1,14 @@
 import * as path from 'path';
 import { NODE_ENV } from '../../constants';
-
+import { logger } from '../../logger/'
 export { NODE_ENV };
+
+const log = logger();
 
 const root = '../../../../';
 
 export const BUNDLE_PATH = NODE_ENV === 'production'
   ? path.resolve(__dirname, root, '../deploy/')
   : path.resolve(__dirname, root, 'build/');
+
+log.info('BUNDLE_PATH', BUNDLE_PATH);
--- src/back/router/middlewares/js-files.ts
+++ src/back/router/middlewares/js-files.ts
@@ -3,13 +3,15 @@ import {
   BUNDLE_PATH,
   NODE_ENV,
 } from './constants';
-
+import { logger } from '../../logger/';
+const log = logger();
 export const jsFiles = ( req: ExpressRequest, res: ExpressResponse): void => {
   res.set('Content-Type', 'text/javascript');
 
   if ( NODE_ENV === 'production' ) {
+    log.log("path.join(BUNDLE_PATH, `${ req.url }.gz`)", path.join(BUNDLE_PATH, `${ req.url }.gz`));
     res.set('Content-Encoding', 'gzip');
-    res.sendFile(path.resolve(BUNDLE_PATH, `${ req.url }.gz`));
+    res.sendFile(path.join(BUNDLE_PATH, `${ req.url }.gz`));
   } else {
     res.sendFile(path.join(BUNDLE_PATH, req.url));
   }
