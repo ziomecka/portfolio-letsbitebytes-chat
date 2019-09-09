6c7d52c98a891ea4b732f33d9c1c5574b8d9327e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-09 08:24:56 +0200

    Common | uses ThemeOptions type

--- src/common/theme/index.ts
+++ src/common/theme/index.ts
@@ -1,16 +1,15 @@
+import createMuiTheme, { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 import { Theme } from '@material-ui/core';
-import { createMuiTheme } from '@material-ui/core/styles';
 import { other } from './other';
 import { palette } from './palette';
 import { typography } from './typography';
 
 const getTheme = (): Theme => {
-  // @ts-ignore
   return createMuiTheme({
     palette,
     typography,
     ...other,
-  });
+  } as ThemeOptions);
 };
 
 export { getTheme };
