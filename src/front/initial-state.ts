108e74fc01816f51b4e98eeaf54507e56be3ffc4
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-11 15:30:03 +0200

    Front | builds initial state on window or common default initial state. Removes destructuring right from the beginning

--- src/front/initial-state.ts
+++ src/front/initial-state.ts
@@ -1,30 +1,12 @@
-import { WINDOW_INITIAL_STATE } from '../common/';
+import {
+  WINDOW_INITIAL_STATE,
+  DEFAULT_INITIAL_STATE as defaultInitialState,
+} from '../common/';
 
-const email = 'initialEmail';
-const isAuthenticated = false;
-const login = 'initialLogin';
-const password = 'initialPassword';
-const role = 'initialRole';
-const connectionState = ConnectionState.unknown;
+const initialState = (window as AppWindow)[ WINDOW_INITIAL_STATE ] as unknown as AppState;
 
-const conversations: Statement[] = [];
-const activeConversation = '';
-const DEFAULT_INITIAL_STATE = {
-  user: {
-    email,
-    isAuthenticated,
-    login,
-    password,
-    role,
-  },
-  conversations,
-  activeConversation,
-  connectionState,
-};
+export const INITIAL_STATE = initialState
+  ? { ...initialState, user: { ...initialState.user } }
+  : { ...defaultInitialState, user: { ...defaultInitialState.user } };
 
-export const INITIAL_STATE = (
-  {
-    ...DEFAULT_INITIAL_STATE,
-    ...(window as AppWindow)[ WINDOW_INITIAL_STATE ] as unknown as AppState,
-  }
-);
+export { defaultInitialState };
