250f1be885c70e43209b8b6687cd4d948fc67426
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-17 21:28:04 +0200

    Front | uses RouterButton in FrontComponent for home location

--- src/front/index.tsx
+++ src/front/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {
+  AppRouter,
+  RouterButton,
+} from './components/';
+import {
   Common,
   HTML_ROOT_ID,
 } from '../common/';
-import { AppRouter } from './components';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PublisherProvider } from 'publisher-subscriber-react-hoc';
@@ -17,26 +20,38 @@ interface FrontComponentProps {
   store: ReduxStore,
 }
 
-export const FrontComponent: React.FunctionComponent<FrontComponentProps> = ({ store }) => (
-  <Provider {...{ store }}>
-    <BrowserRouter>
-      <Common>
-        {
-          // @ts-ignore
-          <PublisherProvider
-            {...{
-              emitter: window,
-              addListenerMethodName: 'addEventListener',
-              removeListenerMethodName: 'removeEventListener',
-            }}
-          >
-            <AppRouter />
-          </PublisherProvider>
-        }
-      </Common>
-    </BrowserRouter>
-  </Provider>
-);
+const HOME_LABEL = 'Home';
+
+export const FrontComponent: React.FunctionComponent<FrontComponentProps> = ({ store }) => {
+  const homeLabel = HOME_LABEL;
+
+  return (
+    <Provider {...{ store }}>
+      <BrowserRouter>
+        <Common>
+          {
+            // @ts-ignore
+            <PublisherProvider
+              {...{
+                emitter: window,
+                addListenerMethodName: 'addEventListener',
+                removeListenerMethodName: 'removeEventListener',
+              }}
+            >
+              <AppRouter />
+              <RouterButton
+                to={ServerRoutes.publicRoute}
+                variant={AppButtonVariant.transparent}
+              >
+                {homeLabel}
+              </RouterButton>
+            </PublisherProvider>
+          }
+        </Common>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 ReactDOM.hydrate(
   <FrontComponent store={store} />,
