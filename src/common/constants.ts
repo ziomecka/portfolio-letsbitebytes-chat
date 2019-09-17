24e44980a09f7ceb82a7cf3d91f42649d5426490
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-17 21:48:04 +0200

    Common | adds login regexp, password regexp, min and max login and password lengths

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -10,3 +10,21 @@ export const AppRoutes = {
   publicRoute: ServerRoutes.publicRoute,
   protectedRoute: ServerRoutes.protectedRoute,
 };
+
+export const MIN_LOGIN_LENGTH = 6;
+export const MAX_LOGIN_LENGTH = 10;
+export const MIN_PASSWORD_LENGTH = 6;
+export const MAX_PASSWORD_LENGTH = 10;
+
+export const SPECIAL_CHARACTERS = '!@#$%^&*()';
+
+// start from letter
+export const LOGIN_REGEXP =
+  new RegExp(`(?=^.{${ MIN_LOGIN_LENGTH },${ MAX_LOGIN_LENGTH }}$)(^[a-z]\\w+$)`, 'i');
+
+// include at least one special character at any position
+export const PASSWORD_REGEXP =
+  new RegExp(
+    `(?=^.{${ MIN_PASSWORD_LENGTH },${ MAX_PASSWORD_LENGTH }}$)(^[\\w${ SPECIAL_CHARACTERS }]*[${ SPECIAL_CHARACTERS }]+[\\w${ SPECIAL_CHARACTERS }]*$)`, // eslint-disable-line max-len
+    'i'
+  );
