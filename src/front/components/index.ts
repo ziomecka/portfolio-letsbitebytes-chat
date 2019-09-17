86672813d10ae147a9ce28782371e339cbdc03b5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-17 21:25:45 +0200

    Front | adds RouterButton

--- src/front/components/index.ts
+++ src/front/components/index.ts
@@ -2,5 +2,6 @@ export { AppRouter } from './AppRouter/';
 export { Conversation } from './Conversation/';
 export { Login } from './Login';
 export { ProtectedRoute } from './ProtectedRoute/';
+export { RouterButton } from './RouterButton/';
 export { Trainee } from './Trainee';
 export { Trainer } from './Trainer';
