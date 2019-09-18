e8aed18d2c3a23caaf74734237abb7eb2d0ea133
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 10:01:03 +0200

    Common | adds create user button in Public component

--- src/common/components/Public/component.tsx
+++ src/common/components/Public/component.tsx
@@ -3,23 +3,41 @@ import { AppButton } from '../AppButton/';
 import { AppRoutes } from '../../../common/constants';
 
 const LOGIN_BUTTON_TEXT = 'Login';
+const CREATE_USER_BUTTON_TEXT = 'Create user';
+
 const Public: React.FunctionComponent<PublicWithRouterProps> = (props) => {
   const loginButtonText = LOGIN_BUTTON_TEXT;
+  const createUserButtonText = CREATE_USER_BUTTON_TEXT;
 
   const goToLogin = (): void => {
     props.history.push(AppRoutes.loginRoute);
   };
 
+  const goToCreateUser = (): void => {
+    props.history.push(AppRoutes.createUserRoute);
+  };
+
   return (
-    <AppButton
-      autoFocus
-      buttonProps={{
-        autoFocus: true,
-        onClick: goToLogin,
-      }}
-    >
-      {loginButtonText}
-    </AppButton>
+    <React.Fragment>
+      <AppButton
+        autoFocus
+        buttonProps={{
+          autoFocus: true,
+          onClick: goToLogin,
+        }}
+      >
+        {loginButtonText}
+      </AppButton>
+      <AppButton
+        autoFocus
+        buttonProps={{
+          autoFocus: true,
+          onClick: goToCreateUser,
+        }}
+      >
+        {createUserButtonText}
+      </AppButton>
+    </React.Fragment>
   );
 };
 
