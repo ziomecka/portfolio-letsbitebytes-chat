f2c857fae9100037f4a04a46bbd4141b5391e98c
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 11:20:16 +0200

    Front | deletes setActiveConversation from async

--- src/front/duck/async.ts
+++
@@ -1,9 +0,0 @@
-import { changeActiveConversation } from './actions';
-import { findPartner } from '../utils/find-partner';
-
-export const setActiveConversation = (role: UserRole): AppThunkAction<CommonActions> => (
-  async (dispatch: AppThunkDispatch<CommonActions>): Promise<ChangeConversationAction> => {
-    const partner = findPartner(role);
-    return dispatch(changeActiveConversation(partner));
-  }
-);
