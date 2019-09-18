d6bed7a415689dae8b7ae5a7d734d523c42d68b9
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 11:47:53 +0200

    Front | adds ReduxDispatch type

--- src/front/_types.d.ts
+++ src/front/_types.d.ts
@@ -1,5 +1,6 @@
 declare type ReduxAction = import('redux').Action;
 declare type ReduxActionCreator<A> = import('redux').ActionCreator<A>;
+declare type ReduxDispatch = import('redux').Dispatch;
 declare type ReduxReducer<S, A> = import('redux').Reducer<S, A>;
 declare type ReduxStore = import('redux').Store;
 
