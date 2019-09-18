79d45d3a78043ef959118b6d88f0f5297d58b2d2
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 10:51:27 +0200

    Back | adds user error

---
+++ src/back/user/_user-error.d.ts
@@ -0,0 +1,8 @@
+declare const enum UserErrors {
+  userExists = 'User already exists',
+  incorrectLogin = 'Login is incorrect',
+  incorrectPassword = 'Password is incorrect',
+  conversationNotFound = 'Conversation not found',
+  messageNotFound = 'Message not found',
+  unknown = 'Something is wrong',
+}
