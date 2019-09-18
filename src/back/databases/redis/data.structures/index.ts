6b4528785375b6fca78b0613a9f1c83bf1f7ce87
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 10:08:38 +0200

    Back | adds redis set methods

--- src/back/databases/redis/data.structures/index.ts
+++ src/back/databases/redis/data.structures/index.ts
@@ -2,10 +2,20 @@ import {
   getString,
   setString,
 } from './string';
+import {
+  sadd,
+  smembers,
+} from './set';
+import { delKey } from './del.key';
 
 export const stringData = {
   delKey,
   getString,
   setString,
+};
+
+export const setData = {
+  sadd,
+  smembers,
 };
 
