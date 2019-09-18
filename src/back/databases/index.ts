be46dff54bf1f973527e16777498b9043960fd44
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 10:38:38 +0200

    Back | adds mongoDB

--- src/back/databases/index.ts
+++ src/back/databases/index.ts
@@ -1,6 +1,8 @@
 import { REDIS_URL } from '../constants';
 import { createClient } from './redis';
 
+export { mongoDB } from './mongo/';
+
 export const redis = createClient(REDIS_URL);
 
 export {
