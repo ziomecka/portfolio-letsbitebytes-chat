0b8a3ec0c1cfd770ef8d96618c60f534634b79c6
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-18 09:59:19 +0200

    Common | adds createUserRoute in AppRoutes

--- src/common/_types.d.ts
+++ src/common/_types.d.ts
@@ -2,6 +2,7 @@ declare interface CommonProps extends WithStyles {}
 declare type WithRouterProps = import('react-router').RouteComponentProps;
 
 declare const enum ServerRoutes {
+  'createUserRoute' = '/createUser',
   'loginRoute' = '/login',
   'logoutRoute' = '/logout',
   'publicRoute' = '/',
