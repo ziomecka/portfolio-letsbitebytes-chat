988412156e137d0ba96685513108d016e7aa2e1e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-19 12:55:58 +0200

    Common | changes theme, adds small spacing to constants

--- src/common/theme/other-constants.ts
+++ src/common/theme/other-constants.ts
@@ -1,5 +1,6 @@
 export const BUTTON_GREY_SHADE = 500; // used for button backgroundColor
 export const BUTTON_SIZE = 8; // used as buttonSize x 1rem
 
+export const SPACING_SMALL = .5;
 export const SPACING_REGULAR = 1;
 export const SPACING_LARGE = 2;
