c157d091d3474ad0ebe86cf77df2fa2cd70d7c68
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-19 22:41:45 +0200

    Back | adds and refactors redis listeners, adds auth

--- src/back/databases/redis/client.ts
+++ src/back/databases/redis/client.ts
@@ -1,30 +1,37 @@
 import * as RedisLib from 'redis';
 import { logger } from '../../logger/';
+const url = require('url');
 
 const log = logger();
 
 export class Redis {
   public client?: RedisLib.RedisClient;
+  private auth: string;
   constructor (redisUrl: string) {
     this.client = undefined;
+    this.auth = url.parse(redisUrl).auth;
+
     this.connect(redisUrl);
   }
 
-  private connect = async (redisUrl: string): Promise<boolean> => (
-    new Promise((resolve, reject): void => {
-      this.client = RedisLib.createClient(redisUrl);
 
-      this.client.on('connect', () => {
-        log.info('Redis connected');
-        resolve(true);
-      });
+  private connect = (redisUrl: string): void => {
+    this.client = RedisLib.createClient(redisUrl);
 
-      this.client.on ('error', (err) => {
-        log.error('Redis connection failed: ', err);
-        reject(err);
-      });
-    })
-  );
+    this.client.on('connect', () => {
+      log.info('Redis connected');
+    });
+
+    this.client.on ('error', (err) => {
+      log.error('Redis connection failed: ', err);
+    });
+
+    this.client.on ('end', () => {
+      log.info('Redis connection ended');
+    });
+
+    this.client.auth(this.auth.split(':')[ 1 ]);
+  };
 }
 
 export const createClient = (url: string): Redis => new Redis(url);
