86f0daee6f0a31f751862495a6d8bd2c86a5cb30
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-19 12:57:58 +0200

    Common | changes theme, adds  maxWidthHeight setting

--- src/common/theme/other.ts
+++ src/common/theme/other.ts
@@ -26,6 +26,13 @@ const other = (theme: Theme): ThemeOptions => {
 
   const marginPadding = buildMarginPadding(regularSpacing, regularSpacing);
 
+  const maxWidthHeight = {
+    maxHeight: '100%',
+    maxWidth: '100%',
+    height: '100%',
+    width: '100%',
+  };
+
   return {
     props: {
       MuiPaper: {
