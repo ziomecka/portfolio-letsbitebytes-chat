f2b1091e6d3bd85e8d5e1fb9962792f7e7bd97b0
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-22 15:39:44 +0200

    Front | moves GetState to Thunk states

--- src/front/_thunk.types.d.ts
+++ src/front/_thunk.types.d.ts
@@ -1,3 +1,5 @@
 declare type AppThunkAction<A, R = A > = import('redux-thunk').ThunkAction<Promise<R>, AppState, {api: Api}, A>;
 
 declare type AppThunkDispatch<A> = import('redux-thunk').ThunkDispatch<AppState, {api: Api}, A>;
+
+declare type GetState = () => AppState;
--- src/front/_types.d.ts
+++ src/front/_types.d.ts
@@ -7,6 +7,3 @@ declare type ReduxStore = import('redux').Store;
 declare type WithStyles = import('@material-ui/core/styles').WithStyles;
 declare type WithPublisherProps = import('publisher-subscriber-react-hoc').WithPublisherProps;
 declare type EventCallback<E> = import('publisher-subscriber-react-hoc').EventCallback<E>;
-
-declare type GetState = () => AppState;
-
