95590f191ab821a3b3796e2d78549abe988505b5
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-22 13:49:39 +0200

    Common | moves Common component to seperate component, inclusdes Provider and PublisherProvider in Common component

---
+++ src/common/components/Common/component.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable no-global-assign */
+import * as React from 'react';
+import {
+  Grid,
+  MuiThemeProvider,
+  Paper,
+  Typography,
+} from '@material-ui/core';
+import { APP_TITLE } from '../../constants';
+import { Provider } from 'react-redux';
+import { PublisherProvider } from 'publisher-subscriber-react-hoc';
+import { getTheme } from '../../theme';
+import { styles } from './styles';
+import { withStyles } from '@material-ui/core/styles';
+
+const windowObject = process.env.IS_BROWSER != 'true'
+  ? {
+    addEventListener: (): void => {},
+    removeEventListener: (): void => {},
+  }
+  : window;
+
+const Common: React.FunctionComponent<CommonProps> = ({
+  children,
+  classes,
+  elevation = 0,
+  login,
+  appTitle = APP_TITLE,
+  store,
+}) => (
+  <Provider store={store} >
+    <MuiThemeProvider theme={ getTheme() }>
+      {/*
+      // @ts-ignore */}
+      <PublisherProvider
+        {...{
+          emitter: windowObject,
+          addListenerMethodName: 'addEventListener',
+          removeListenerMethodName: 'removeEventListener',
+        }}
+      >
+        <Grid
+          container
+          className={classes.rootBox}
+          component={Paper}
+          elevation={elevation}
+          justify="center"
+        >
+          <Typography variant="h1">
+            { `${ appTitle } ${ login && `, ${ login }!` }` }
+          </Typography>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            classes={{ root: classes.box }}
+          >
+            { children }
+          </Grid>
+        </Grid>
+      </PublisherProvider>
+    </MuiThemeProvider>
+  </Provider>
+);
+
+const WrappedCommon = withStyles(styles)(Common);
+
+export { WrappedCommon as Common };
--- src/common/components/Common/styles.ts
+++ src/common/components/Common/styles.ts
@@ -1,7 +1,9 @@
-import { createStyles } from '@material-ui/core/styles';
-import { fontSizes } from './theme/typography-constants';
+import {
+  Theme,
+  createStyles,
+} from '@material-ui/core/styles';
 
-export const styles = createStyles(() => ({
+export const styles = createStyles(({ typography }: Theme) => ({
   rootBox: {
     margin: 0,
     width: '100%',
@@ -10,7 +12,7 @@ export const styles = createStyles(() => ({
   },
   box: {
     // * 2 => h1.fontSize + h1 marginBottom (overrides MuiTypography, root, h1, marginBottom)
-    height: `calc(100% - ${ fontSizes.h1 } * 2)`,
+    height: `calc(100% - ${ typography.h1.fontSize } * 2)`,
     width: '100%',
   },
 }
