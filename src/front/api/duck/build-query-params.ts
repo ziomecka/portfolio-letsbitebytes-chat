ef93545f860cd8592021e8af1fccd5d621eba98c
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-22 21:23:12 +0200

    Front | deletes unnecessary build-query-params

--- src/front/api/duck/build-query-params.ts
+++
@@ -1,6 +0,0 @@
-export const buildQueryParams = (url: string, params: unknown): string => (
-  `${ url }?${
-    Object.entries(params as Record<string, string>)
-      .map(([ key, value ]) => `${ key }=${ value || undefined }`).join('&')
-  }`
-);
