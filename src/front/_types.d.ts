a1fdee70121de7212d59635e1ffa3ff99e3c8860
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-23 09:28:15 +0200

    Front | deletes publisher-subscriber-react-hoc types

--- src/front/_types.d.ts
+++ src/front/_types.d.ts
@@ -5,5 +5,3 @@ declare type ReduxReducer<S, A> = import('redux').Reducer<S, A>;
 declare type ReduxStore = import('redux').Store;
 
 declare type WithStyles = import('@material-ui/core/styles').WithStyles;
-declare type WithPublisherProps = import('publisher-subscriber-react-hoc').WithPublisherProps;
-declare type EventCallback<E> = import('publisher-subscriber-react-hoc').EventCallback<E>;
