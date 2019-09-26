e4bdc52752e1a416de18766abce9e2ed896cd090
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-26 08:58:14 +0200

    Common | renders AppMenu in ProtectedRoute, Logout deleted as rendered in AppMenu

--- src/common/components/ProtectedRoute/styles.ts
+++ src/common/components/ProtectedRoute/styles.ts
@@ -1,20 +1,14 @@
-import { Theme } from '@material-ui/core/styles';
-import { createStyles } from '@material-ui/styles';
+import { Theme, createStyles } from '@material-ui/core';
 
 export const styles = createStyles(({
   shape: { borderRadius },
 }: Theme) => {
-  const boxHeight = '80px'; // todo calculate from theme ?
-
   return {
     box: {
       borderRadius,
-      height: `calc(100% - ${ boxHeight })`,
       overflow: 'hidden',
       width: 'auto',
     },
-    buttonsBox: {
-      height: boxHeight,
     },
   };
 });
