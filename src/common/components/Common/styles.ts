1807ab2b314b381008c2c2764d1fabd5743b7a79
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-26 09:19:52 +0200

    Common | adds app minHeight, maxWidth and margin auto to the rootBox in Common component

--- src/common/components/Common/styles.ts
+++ src/common/components/Common/styles.ts
@@ -1,21 +1,38 @@
 import {
+  APP_MAX_WIDTH,
+  APP_MIN_HEIGHT,
+  SPACING_LARGE,
+} from '../../theme/other-constants';
+import {
   Theme,
   createStyles,
 } from '@material-ui/core/styles';
-import { SPACING_LARGE } from '../../theme/other-constants';
 
-export const styles = createStyles(({ spacing, typography }: Theme) => ({
-  rootBox: {
-    position: 'relative',
-    margin: 0,
-    padding: spacing(SPACING_LARGE),
-    width: '100%',
-    height: '100%',
-    overflow: 'hidden',
-  },
-  box: {
-    // * 2 => h1.fontSize + h1 marginBottom (overrides MuiTypography, root, h1, marginBottom)
-    height: `calc(100% - ${ typography.h1.fontSize } * 2)`,
-    width: '100%',
-  },
-}));
+export const styles = createStyles(({
+  breakpoints,
+  spacing,
+  typography,
+}: Theme) => {
+  const largeSpacing = spacing(SPACING_LARGE);
+
+  return {
+    rootBox: {
+      position: 'relative',
+      margin: '0 auto',
+      padding: largeSpacing,
+      maxWidth: APP_MAX_WIDTH,
+      width: '100%',
+      maxHeight: '100vh',
+      height: '100vh',
+      [ breakpoints.up('md') ]: {
+        height: APP_MIN_HEIGHT,
+      },
+      overflow: 'hidden',
+    },
+    box: {
+      // * 2 => h1.fontSize + h1 marginBottom (overrides MuiTypography, root, h1, marginBottom)
+      height: `calc(100% - ${ typography.h1.fontSize } * 2)`,
+      width: '100%',
+    },
+  };
+});
