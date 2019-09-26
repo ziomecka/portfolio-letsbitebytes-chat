74310d8fe24d4e56bb7d57c5635b4ba4fa20ddea
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-26 08:50:40 +0200

    Common | adds Route to ProtectedRoute component, deletes Users component as rendered in conversation, removes Paper component

--- src/common/components/ProtectedRoute/component.tsx
+++ src/common/components/ProtectedRoute/component.tsx
@@ -1,61 +1,53 @@
 import * as React from 'react';
 import {
-  Box,
-  Grid,
-  Paper,
-} from '@material-ui/core';
-import { Conversation } from '../Conversation/';
 import { Logout } from '../Logout/';
-import { Redirect } from 'react-router-dom';
-import { Socket } from '../Socket/';
-import { Users } from '../Users/';
+  Conversation,
+  Socket,
+  Users,
+} from '../';
+import {
+  Redirect,
+  Route,
+} from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 import { styles } from './styles';
 import { withStyles } from '@material-ui/styles/';
 
 const ProtectedRoute: React.FunctionComponent<ProtectedRouteProps> =
 ({
-  isAuthenticated,
   classes,
-  elevation = 0,
+  isAuthenticated,
+  path,
 }) => {
-  return (isAuthenticated ? (
-    <Grid
-      container
-      justify="space-between"
-      component={Paper}
-      elevation={elevation}
-    >
+
+  const renderComponent = (): JSX.Element => (
+    isAuthenticated ? (
       <Grid
-        item
-        component={Box}
+        container
         className={classes.box}
-        xs={4}
-      >
-        <Users />
-      </Grid>
-      <Grid
-        item
-        component={Box}
-        className={classes.box}
-        xs={7}
+        justify="space-between"
       >
         <Conversation />
+        <Grid
+          item
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          className={classes.buttonsBox}
+        >
+          <Logout/>
+        </Grid>
+        <Socket />
       </Grid>
-      <Grid
-        item
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        className={classes.buttonsBox}
-      >
-        <Logout/>
-      </Grid>
-      <Socket />
-    </Grid>
-  ) : (
-    <Redirect to={{ pathname: '', state: {} }}/>
-  ));
+    ) : (
+      <Redirect to={{ pathname: '', state: {} }}/>
+    )
+  );
+
+  return (
+    <Route exact path={path} render={renderComponent} />
+  );
 };
 
 const WrappedComponent = withStyles(styles)(ProtectedRoute);
