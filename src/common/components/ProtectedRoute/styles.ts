375b5be9cd3745fe88b8cfbd5fc1f2a2c0686cdf
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-26 16:11:53 +0200

    Common | uses Box component in ProtectedRoute and adds paddingTop

--- src/common/components/ProtectedRoute/styles.ts
+++ src/common/components/ProtectedRoute/styles.ts
@@ -1,15 +1,24 @@
+import {
+  APP_NAV_HEIGHT,
+  SPACING_LARGE,
+  SPACING_REGULAR,
+} from '../../theme/other-constants';
 import { Theme, createStyles } from '@material-ui/core';
-import { APP_NAV_HEIGHT } from '../../theme/other-constants';
 
 export const styles = createStyles(({
   breakpoints,
   spacing,
 }: Theme) => {
+  const padding = spacing(SPACING_REGULAR);
+  const paddingTop = spacing(SPACING_LARGE);
+
   return {
     box: {
-      height: `calc(100% - ${ APP_NAV_HEIGHT + spacing() }px)`,
+      height: `calc(100% - ${ APP_NAV_HEIGHT }px)`,
+      paddingTop,
       [ breakpoints.up('md') ]: {
-        padding: `0 ${ spacing() }px`,
+        padding: `0 ${ padding }px`,
+        paddingTop,
       },
     },
   };
