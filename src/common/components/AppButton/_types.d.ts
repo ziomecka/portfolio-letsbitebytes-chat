5268fcf7bba6057bf69c2777853d2108e1506b55
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-26 16:19:54 +0200

    Common | removes AppButtonVariants from AppButton as Mui button variants are used, adds primary color as default to AppButton, makes buttonProps a Partial in AppButtonProps interface

--- src/common/components/AppButton/_types.d.ts
+++ src/common/components/AppButton/_types.d.ts
@@ -1,12 +1,4 @@
-type ButtonProps = import('@material-ui/core/Button').ButtonProps
-
-declare const enum  AppButtonVariant {
-  transparent = 'transparent',
-  flex = 'flex',
-}
-
 declare interface AppButtonProps extends WithStyles {
   autoFocus?: boolean;
-  buttonProps?: ButtonProps;
-  variant?: AppButtonVariant;
+  buttonProps?: Partial<import('@material-ui/core/Button').ButtonProps>;
 }
