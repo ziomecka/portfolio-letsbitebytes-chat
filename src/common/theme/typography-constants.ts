f0a1cb2c06aa2e7cd98b0854e1265fc7bafbb173
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-28 18:42:54 +0200

    Common | refactors theme typography constants, fontWeights, lineHeights and letterSpacings grouped in objects

--- src/common/theme/typography-constants.ts
+++ src/common/theme/typography-constants.ts
@@ -10,18 +10,24 @@ export const fontFamily = '"Roboto", "Helvetica", "Arial", sans-serif';
 
 export const fontSize = 16;
 
-export const fontWeightLight = 400;
-export const fontWeightMedium = 500;
-export const fontWeightRegular = 500;
-export const fontWeightBold = 600;
+export const fontWeights = {
+  fontWeightLight: 400,
+  fontWeightMedium: 500,
+  fontWeightRegular: 500,
+  fontWeightBold: 600,
+  button: 400,
+  heading: 400,
+};
 
-export const lineHeight = '1.5em';
-export const headingLineHeight = '1.2em';
+export const lineHeights = {
+  lineHeight: '1.5em',
+  heading: '1.2em',
+  button: '1.5em',
+};
 
-export const fontWeightHeading = 400;
-export const letterSpacingHeading = '-.02em';
+export const letterSpacings = {
+  heading: '-.02em',
+  button: '-.01em',
+};
 
-export const fontWeightButton = 400;
-export const letterSpacingButton = '-.01em';
-export const lineHeightButton = '1.5em';
 export const textTransformButton = 'none';
