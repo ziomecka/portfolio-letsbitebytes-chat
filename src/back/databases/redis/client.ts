df32e90f53a21eefd74d52aed28fda35b0f2542f
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-29 22:10:49 +0200

    Back | extends RedisClient in Redis class

--- src/back/databases/redis/client.ts
+++ src/back/databases/redis/client.ts
@@ -1,37 +1,30 @@
 import * as RedisLib from 'redis';
 import { logger } from '../../logger/';
-const url = require('url');
 
 const log = logger();
 
-export class Redis {
-  public client?: RedisLib.RedisClient;
-  private auth: string;
+export class Redis extends RedisLib.RedisClient {
   constructor (redisUrl: string) {
-    this.client = undefined;
-    this.auth = url.parse(redisUrl).auth;
-
-    this.connect(redisUrl);
+    super({ url: redisUrl });
+    this.init();
   }
 
   public disconnect = async (): Promise<boolean> => {
     return new Promise((resolve): boolean => (
-      this.client.quit(() => resolve(true))
+      this.quit(() => resolve(true))
     ));
   }
 
-  private connect = (redisUrl: string): void => {
-    this.client = RedisLib.createClient(redisUrl);
-
-    this.client.on('connect', () => {
+  private init = (): void => {
+    this.on('connect', () => {
       log.info('Redis connected');
     });
 
-    this.client.on ('error', (err) => {
+    this.on('error', (err) => {
       log.error('Redis connection failed: ', err);
     });
 
-    this.client.on ('end', () => {
+    this.on('end', () => {
       log.info('Redis connection ended');
     });
   };
