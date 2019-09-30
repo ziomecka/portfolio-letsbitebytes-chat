f650915a538eb786a5101016c588534bf5a2e68a
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-30 09:22:07 +0200

    Back | promisifies Redis methods

--- src/back/databases/redis/index.ts
+++ src/back/databases/redis/index.ts
@@ -1,8 +1,10 @@
 import * as RedisLib from 'redis';
 import { logger } from '../../logger/';
+import { promisify } from '../../utils/promisify';
 
 const url = require('url');
 const log = logger();
+const redisLogger = logger('redis');
 
 export class Redis extends RedisLib.RedisClient {
   constructor (redisUrl: string) {
@@ -22,7 +24,26 @@ export class Redis extends RedisLib.RedisClient {
     ));
   }
 
+  private promisifyMethods (): void {
+    const not = ['multi'];
+
+    Object.keys(this).forEach(key => {
+      if (
+        Object.hasOwnProperty.call(this.client, key) &&
+        // @ts-ignore
+        typeof this.client[ key ] === 'function' &&
+        // @ts-ignore
+        !not.includes(this.client[ key ].name)
+      ) {
+        // @ts-ignore
+        this[ key ] = promisify(this.client[ key ], redisLogger);
+      }
+    });
+  }
+
   private init = (): void => {
+    this.promisifyMethods();
+
     this.on('connect', () => {
       log.info('Redis connected');
     });
