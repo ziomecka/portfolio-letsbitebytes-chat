4dcac081c92e609387ff21a6b0a29bb06926b0ad
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2019-09-30 08:16:44 +0200

    Back | moves redis client to index file

--- src/back/databases/redis/index.ts
+++ src/back/databases/redis/index.ts
@@ -1,9 +1,33 @@
-export {
-  setData,
-  stringData,
-} from './data.structures/';
+import * as RedisLib from 'redis';
+import { logger } from '../../logger/';
 
-export {
-  Redis,
-  createRedis,
-} from './client';
+const log = logger();
+
+export class Redis extends RedisLib.RedisClient {
+  constructor (redisUrl: string) {
+    super({ url: redisUrl });
+    this.init();
+  }
+
+  public disconnect = async (): Promise<boolean> => {
+    return new Promise((resolve): boolean => (
+      this.quit(() => resolve(true))
+    ));
+  }
+
+  private init = (): void => {
+    this.on('connect', () => {
+      log.info('Redis connected');
+    });
+
+    this.on('error', (err) => {
+      log.error('Redis connection failed: ', err);
+    });
+
+    this.on('end', () => {
+      log.info('Redis connection ended');
+    });
+  };
+}
+
+export const createRedis = (url: string): Redis => new Redis(url);
